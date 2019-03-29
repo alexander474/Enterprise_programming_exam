a6a5d2b6285fb5394eaa31a32ec421af4a1513cf
blacktype <breale17@student.westerdals.no>
2019-03-30 00:59:50 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Enterprise_programming_exercise
---
+++ backend/src/test/java/no/ab/moc/StubApplication.java
@@ -0,0 +1,10 @@
+package no.ab.moc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by arcuri82 on 14-Dec-17.
+ */
+@SpringBootApplication
+public class StubApplication {
+}
